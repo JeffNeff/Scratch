951bb26f19bddb1b9cfb8514d9b307dd8423dc3e
Jeffrey Naef <60796676+JeffNeff@users.noreply.github.com>
2022-08-01 23:35:08 -0700

    Contract Comments.

--- packages/hardhat/contracts/Lottery.sol
+++ packages/hardhat/contracts/Lottery.sol
@@ -5,22 +5,31 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Lottery is Ownable{
-    //new player in the contract using array[] to unlimit number
+    // players contains the address of the current players
     address[] public players;
-    //Leaderboard of the contract containing the address of the player and the number of the tokens they have won.
+    // Leaderboard of the contract containing the address of the player and the number of the tokens they have won.
     uint[] public  leaderboardWinnings;
+    // leaderboardPlayers contains the address of the players in the leaderboard.
     address[] public  leaderboardPlayers;
+    // leaderboardCount contains the number of players in the leaderboard.
     uint public leaderboardCount;
-
+    // _totalSupply contains the total number of tokens in the contract.
     uint256 private _totalSupply;
+    // _lifetimeWinnings contains the total number of tokens won players.
     uint256 private _lifetimeWinnings;
 
+    // NewPlayer is an event that is emited when a new player joins the contract.
     event NewPlayer(address player);
 
+    // depositEth() is a public function that allows the player to deposit tokens into the contract to enter the game.
     function depositEth() public payable {
+        // The player is required to send > 10 tokens to play.
         require(msg.value >= 10 ether, 'You ow more money');
+        // The player is added to the players array.
         players.push(payable(msg.sender));
+        // the totalSupply is increased by the amount of tokens sent.
         _totalSupply += msg.value;
+        // The NewPlayer event is emited.
         emit NewPlayer(msg.sender);
     }
 
@@ -39,24 +48,31 @@ contract Lottery is Ownable{
         return _totalSupply;
     }
 
-    //creates a random hash that will become our winner
+    // random() is a private function that returns a random number between 0 and the number of players.
     function random() private view returns(uint){
         return  uint (keccak256(abi.encode(block.timestamp,  players)));
     }
 
+    // addLeaderbaordWinner() is a private function that adds a player to the leaderboard.
     function addLeaderbaordWinner(address player, uint256 winnings) private {
+        // illterate through the leaderboardPlayers array.
        for (uint i = 0; i < leaderboardCount; i++) {
+            // if the player is already in the leaderboard, the winnings are added to the existing winnings.
             if (leaderboardPlayers[i] == player) {
                 leaderboardWinnings[i] += winnings;
                 return;
             }
         }
+        // if the player is not in the leaderboard, the player is added to the leaderboard.
         leaderboardPlayers.push(player);
+        // the winnings are added to the leaderboardWinnings array.
         leaderboardWinnings.push(winnings);
+        // the leaderboardCount is increased by 1.
         leaderboardCount++;
     }
 
-    // function pickWinner() public restricted{
+    // pickWinner() is a public function that only the owner can call.
+    // this function selects a winner from the [] of players.
     function pickWinner() public onlyOwner {
         //creates index that is gotten from func random % play.len
         uint index = random() % players.length;
@@ -75,5 +91,4 @@ contract Lottery is Ownable{
         // emty the totalSupply
         _totalSupply = 0;
     }
-
 }
